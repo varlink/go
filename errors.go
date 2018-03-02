de83ff9cdaf92dd0d0204dc0ebd4078ca10d1a50
Harald Hoyer <harald@redhat.com>
2018-03-02 13:22:01 +0100

    move varlink errors to errors.go

---
+++ errors.go
@@ -0,0 +1,41 @@
+package varlink
+
+func InterfaceNotFound(ctx Context, name string) error {
+	type ReplyParameters struct {
+		Name string `json:"interface"`
+	}
+	return ctx.Reply(&ServerReply{
+		Error:      "org.varlink.service.InterfaceNotFound",
+		Parameters: ReplyParameters{Name: name},
+	})
+}
+
+func MethodNotFound(ctx Context, name string) error {
+	type ReplyParameters struct {
+		Name string `json:"method"`
+	}
+	return ctx.Reply(&ServerReply{
+		Error:      "org.varlink.service.MethodNotFound",
+		Parameters: ReplyParameters{Name: name},
+	})
+}
+
+func MethodNotImplemented(ctx Context, name string) error {
+	type ReplyParameters struct {
+		Name string `json:"method"`
+	}
+	return ctx.Reply(&ServerReply{
+		Error:      "org.varlink.service.MethodNotImplemented",
+		Parameters: ReplyParameters{Name: name},
+	})
+}
+
+func InvalidParameter(ctx Context, parameter string) error {
+	type ReplyParameters struct {
+		Parameter string `json:"parameter"`
+	}
+	return ctx.Reply(&ServerReply{
+		Error:      "org.varlink.service.InvalidParameter",
+		Parameters: ReplyParameters{Parameter: parameter},
+	})
+}
