757caf2f3bd6738e9bea3b09758e861bd5423b28
Harald Hoyer <harald@redhat.com>
2018-03-07 13:39:13 +0100

    more IDL tests

--- cmd/varlink-generator/generator_test.go
+++ cmd/varlink-generator/generator_test.go
@@ -121,6 +121,12 @@ func TestDomainNames(t *testing.T) {
 	testParse(t, false, "interface Co9.example.number-toplevel\nmethod F()->()")
 	testParse(t, false, "interface 1om.example.number-toplevel\nmethod F()->()")
 	testParse(t, false, "interface com.Example\nmethod F()->()")
+	var name string
+	for i := 0; i < 255; i++ {
+		name += "a"
+	}
+	testParse(t, false, "interface com.example.toolong"+name+"\nmethod F()->()")
+	testParse(t, false, "interface xn--example.toolong"+name+"\nmethod F()->()")
 }
 
 func TestNoMethod(t *testing.T) {
@@ -145,6 +151,30 @@ func TestTypeOneArray(t *testing.T) {
 	testParse(t, false, "interface foo.bar\n type I (b:bool[1])\nmethod  F()->()")
 	testParse(t, false, "interface foo.bar\n type I (b:bool[ 1 ])\nmethod  F()->()")
 	testParse(t, false, "interface foo.bar\n type I (b:bool[ 1 1 ])\nmethod  F()->()")
+}
+
+func TestFieldnames(t *testing.T) {
+	testParse(t, false, "interface foo.bar\n type I (Test:bool[])\nmethod  F()->()")
+	testParse(t, false, "interface foo.bar\n type I (_test:bool[])\nmethod  F()->()")
+	testParse(t, false, "interface foo.bar\n type I (Äest:bool[])\nmethod  F()->()")
+}
+
+func TestEnum(t *testing.T) {
+	testParse(t, true, "interface foo.bar\n type I (b:(foo, bar, baz))\nmethod  F()->()")
+}
+
+func TestIncomplete(t *testing.T) {
+	testParse(t, false, "interfacef foo.bar\nmethod  F()->()")
+	testParse(t, false, "interface foo.bar\nmethod  F()->()\ntype I (b: bool")
+	testParse(t, false, "interface foo.bar\nmethod  F()->(")
+	testParse(t, false, "interface foo.bar\nmethod  F(")
+	testParse(t, false, "interface foo.bar\nmethod  ()->()")
+	testParse(t, false, "interface foo.bar\nmethod  F->()\n")
+	testParse(t, false, "interface foo.bar\nmethod  F()->\n")
+	testParse(t, false, "interface foo.bar\nmethod  F()>()\n")
+	testParse(t, false, "interface foo.bar\nmethod  F()->()\ntype (b: bool)")
+	testParse(t, false, "interface foo.bar\nmethod  F()->()\nerror (b: bool)")
+	testParse(t, false, "interface foo.bar\nmethod  F()->()\n dfghdrg")
 }
 
 func TestDuplicate(t *testing.T) {
