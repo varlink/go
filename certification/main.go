b10d554fec5e16b7dd9b8216052a9328371cfe3b
Harald Hoyer <harald@harald-hoyer.de>
2018-04-26 08:59:41 +0200

    satisfy $ go vet ./...

--- certification/main.go
+++ certification/main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/varlink/go/varlink"
 	"github.com/varlink/go/certification/orgvarlinkcertification"
+	"github.com/varlink/go/varlink"
 	"os"
 )
 
@@ -16,7 +16,7 @@ func main() {
 
 	c, err := varlink.NewConnection(address)
 	if err != nil {
-		fmt.Println("Failed to connect\n")
+		fmt.Println("Failed to connect")
 		return
 	}
 
@@ -24,28 +24,28 @@ func main() {
 
 	err = orgvarlinkcertification.Start(c, false, false)
 	if err != nil {
-		fmt.Println("Start() failed\n")
+		fmt.Println("Start() failed")
 		return
 	}
 
 	var client_id string
 	_, err = orgvarlinkcertification.ReadStart_(c, &client_id)
 	if err != nil {
-		fmt.Println("StartRead() failed\n")
+		fmt.Println("StartRead() failed")
 		return
 	}
 	fmt.Println("Start: " + client_id + "\n")
 
 	err = orgvarlinkcertification.Test01(c, false, false, client_id)
 	if err != nil {
-		fmt.Println("Test01() failed\n")
+		fmt.Println("Test01() failed")
 		return
 	}
 
 	var flag bool
 	_, err = orgvarlinkcertification.ReadTest01_(c, &flag)
 	if err != nil {
-		fmt.Println("ReadTest01() failed\n")
+		fmt.Println("ReadTest01() failed")
 		return
 	}
 	fmt.Printf("Test01: %t\n", flag)
