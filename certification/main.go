5b91cb4e5a06125bf5bca887f79518768fb69929
Kay Sievers <kay@vrfy.org>
2018-04-29 13:06:25 +0200

    Update certification test

--- certification/main.go
+++ certification/main.go
@@ -33,63 +33,63 @@ func main() {
 		fmt.Println("Test01() failed")
 		return
 	}
-	fmt.Printf("Test01: '%t'\n", b1)
+	fmt.Printf("Test01: '%v'\n", b1)
 
 	i2, err := orgvarlinkcertification.Test02().Call(c, client_id, b1)
 	if err != nil {
 		fmt.Println("Test02() failed")
 		return
 	}
-	fmt.Printf("Test02: '%t'\n", i2)
+	fmt.Printf("Test02: '%v'\n", i2)
 
 	f3, err := orgvarlinkcertification.Test03().Call(c, client_id, i2)
 	if err != nil {
 		fmt.Println("Test03() failed")
 		return
 	}
-	fmt.Printf("Test03: '%t'\n", f3)
+	fmt.Printf("Test03: '%v'\n", f3)
 
 	s4, err := orgvarlinkcertification.Test04().Call(c, client_id, f3)
 	if err != nil {
 		fmt.Println("Test04() failed")
 		return
 	}
-	fmt.Printf("Test04: '%t'\n", s4)
+	fmt.Printf("Test04: '%v'\n", s4)
 
 	b5, i5, f5, s5, err := orgvarlinkcertification.Test05().Call(c, client_id, s4)
 	if err != nil {
 		fmt.Println("Test05() failed")
 		return
 	}
-	fmt.Printf("Test05: '%t'\n", b5)
+	fmt.Printf("Test05: '%v'\n", b5)
 
 	o6, err := orgvarlinkcertification.Test06().Call(c, client_id, b5, i5, f5, s5)
 	if err != nil {
 		fmt.Println("Test06() failed")
 		return
 	}
-	fmt.Printf("Test06: '%t'\n", o6)
+	fmt.Printf("Test06: '%v'\n", o6)
 
 	m7, err := orgvarlinkcertification.Test07().Call(c, client_id, o6)
 	if err != nil {
 		fmt.Println("Test07() failed")
 		return
 	}
-	fmt.Printf("Test07: '%t'\n", m7)
+	fmt.Printf("Test07: '%v'\n", m7)
 
 	m8, err := orgvarlinkcertification.Test08().Call(c, client_id, m7)
 	if err != nil {
 		fmt.Println("Test08() failed")
 		return
 	}
-	fmt.Printf("Test08: '%t'\n", m8)
+	fmt.Printf("Test08: '%v'\n", m8)
 
 	t9, err := orgvarlinkcertification.Test09().Call(c, client_id, m8)
 	if err != nil {
 		fmt.Println("Test09() failed")
 		return
 	}
-	fmt.Printf("Test09: '%t'\n", t9)
+	fmt.Printf("Test09: '%v'\n", t9)
 
 	receive10, err := orgvarlinkcertification.Test10().Send(c, varlink.More, client_id, t9)
 	if err != nil {
@@ -103,7 +103,7 @@ func main() {
 			fmt.Println("Test10() receive failed")
 			return
 		}
-		fmt.Printf("Test10: '%t'\n", s10)
+		fmt.Printf("Test10: '%v'\n", s10)
 
 		if flags10 & varlink.Continues == 0 {
 			break;
